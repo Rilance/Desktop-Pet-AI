9029d50ca83d453812e2a11a05c405a7abecc52f
Lin-Silver <143802258+Lin-Silver@users.noreply.github.com>
2024-08-20 05:41:16 -0400

    Add files via upload

---
+++ LLM/API.bat
@@ -0,0 +1,2 @@
+.\venv\Scripts\python server.py --model-dir I:\text-generation-webui-main\models --model llama-2-7b-chat.Q4_K_S.gguf --listen --listen-port 7860 --character Copilot --gradio-auth Raiden:13252123393Wf --api
+pause
