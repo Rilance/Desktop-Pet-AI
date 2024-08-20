4de0621564ceff8f171b08fc9f440094c062319c
Lin-Silver <143802258+Lin-Silver@users.noreply.github.com>
2024-08-20 05:31:21 -0400

    Add files via upload

---
+++ TTS/API-EN.bat
@@ -0,0 +1,2 @@
+.\venv\Scripts\python.exe api.py -dr "2.wav" -dt "System reset complete.I am a type 3 general AI equipped with sequential integration.Development code, fairy." -dl "en"
+pause
---
+++ TTS/API-ZH.bat
@@ -0,0 +1,2 @@
+.\venv\Scripts\python.exe api.py -dr "1.wav" -dt "了解，我会如同主人使唤我一般使唤它，并且让它不敢有怨言。" -dl "zh"
+pause
