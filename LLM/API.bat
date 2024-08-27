283d5ab6ea153029a01f5df196c6d306ad6193da
Lin-Silver <143802258+Lin-Silver@users.noreply.github.com>
2024-08-27 04:38:26 -0400

    Update API.bat

--- LLM/API.bat
+++ LLM/API.bat
@@ -1,2 +1,2 @@
-.\venv\Scripts\python server.py --model-dir I:\text-generation-webui-main\models --model llama-2-7b-chat.Q4_K_S.gguf --listen --listen-port 7860 --character Copilot --gradio-auth Raiden:13252123393Wf --api
+.\venv\Scripts\python server.py --model-dir "models" --model llama-2-7b-chat.Q4_K_S.gguf --listen --listen-port 7860 --character Copilot --gradio-auth Raiden:13252123393Wf --api
 pause
