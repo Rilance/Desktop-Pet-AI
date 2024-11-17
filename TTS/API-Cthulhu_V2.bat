c048d7b1fe1a1ce667f5841afc36fba4e300ba6f
Lin-Silver <143802258+Lin-Silver@users.noreply.github.com>
2024-11-17 18:54:56 +0800

    Add files via upload

---
+++ TTS/API-Cthulhu_V2.bat
@@ -0,0 +1,2 @@
+.\venv\Scripts\python.exe api.py -a "127.0.0.1" -dr "4.wav" -dt "似乎对人类很珍贵，不论生存还是毁灭。" -dl "zh" -s ".\SoVITS_weights_v2\Cthulhu_e8_s40.pth" -g ".\GPT_weights_v2\Cthulhu-e48.ckpt"
+pause
