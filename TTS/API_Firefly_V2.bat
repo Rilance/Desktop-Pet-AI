73fb7bb94512ba3e80e711c65d94d2b3f703364a
Lin-Silver <143802258+Lin-Silver@users.noreply.github.com>
2024-11-17 19:37:02 +0800

    Add files via upload

---
+++ TTS/API_Firefly_V2.bat
@@ -0,0 +1,2 @@
+.\venv\Scripts\python.exe api.py -a "127.0.0.1" -dr "3.wav" -dt "Elio can see his own future, and I can also see mine." -dl "en" -s ".\SoVITS_weights_v2\Firefly_V2_e8_s48.pth" -g ".\GPT_weights_v2\Firefly_V2-e45.ckpt"
+pause
