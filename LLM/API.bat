71fc7e8d9e9a56e48dc24b4504b84431bcf587eb
Lin-Silver <143802258+Lin-Silver@users.noreply.github.com>
2024-11-17 05:39:59 -0500

    Delete LLM directory

--- LLM/API.bat
+++
@@ -1,2 +0,0 @@
-.\venv\Scripts\python server.py --model-dir "models" --model llama-2-7b-chat.Q4_K_S.gguf --listen-host 127.0.0.1 --listen-port 7860 --character Copilot --gradio-auth Raiden:13252123393Wf --api
-pause
